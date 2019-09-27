1f8cf15d6dee1614d49935798e32d5f370dab8f2
Mastermind497 <39387439+Mastermind497@users.noreply.github.com>
2019-09-27 17:47:40 -0400

    Added Claw DcMotor, changed ServoOne to clawServo

--- Skystone 2019-2020/RobotHardware.java
+++ Skystone 2019-2020/RobotHardware.java
@@ -1,28 +1,26 @@
-/* 
- * The Robot Hardware code for the Destriers
+/*
+ * The Robot Hardware code for the Destriers/FIRE Team
  * Created by Team Member Shourya Bansal
  */
-
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.GyroSensor;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class RobotHardware {
     //Declaring the specific hardware for our robot
     //The DC Motors:
-    public DcMotor rightMotor;
-    public DcMotor leftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor frontLeftMotor;
+
+    public DcMotor clawMotor;
     //Add more DC Motors when you use them
 
     //The Servos
-    public Servo servoOne;
+    public Servo clawServo;
     public Servo servoTwo;
 
     //The Color Sensors
@@ -35,6 +33,7 @@ public class RobotHardware {
     public DistanceSensor distanceSensor;
 
     public static final double SERVO_INIT_POS = 0.5;//This is the initial position of a servo and what you will send it back to
+    public static final double SERVO_OPEN_POS = 45;
 
     HardwareMap hardwareMap;
 
@@ -43,29 +42,35 @@ public class RobotHardware {
         hardwareMap = aHardwareMap;
 
         //Initializing Motors
-        leftMotor = hardwareMap.get(DcMotor.class, "Front Left Motor");
-        rightMotor = hardwareMap.get(DcMotor.class, "Front Right Motor");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "Front Left Motor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "Front Right Motor");
+        clawMotor = hardwareMap.get(DcMotor.class, "Claw Motor");
 
         //Set Directions
-        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        clawMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+
 
         //Set Motor Power to Zero
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        clawMotor.setPower(0);
 
         //Run with Encoders. If we don't use Encoders, change "RUN_USING_ENCODERS" to "RUN_WITHOUT_ENCODERS"
-        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        clawMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         //Brakes the Motors when the power is at Zero
-        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        clawMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         //Define and Initialize Servos
-        servoOne = hardwareMap.get(Servo.class, "<Servo One Name>");
+        clawServo = hardwareMap.get(Servo.class, "<Servo One Name>");
         servoTwo = hardwareMap.get(Servo.class, "<Servo Two Name>");
-        servoOne.setPosition(SERVO_INIT_POS);
+        clawServo.setPosition(SERVO_INIT_POS);
         servoTwo.setPosition(SERVO_INIT_POS);
 
         //Define a Color Sensor
